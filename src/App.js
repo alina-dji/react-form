0f35d67c86a3245a321212339843c6545864ec04
Lina Djihane <lina.djihane@gmail.com>
2021-02-09 10:27:13 +0100

    n

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Contact Us</h1>
+        <p>Got a question? We'd love to hear from you. Send us a message and we'll respond as soon as possible.</p>
+        <Form>
+          <FormGroup>
+              <Input placeholder="Name"/>
+          </FormGroup>
+          <FormGroup>
+            <Input type="email" name="email" id="exampleEmail" placeholder="E-mail" />
+          </FormGroup>
+          <FormGroup>
+            <Input type="textarea" placeholder="Message"/>
+          </FormGroup>
+          <Button color="secondary" block>Send Message</Button>
+        </Form>
+      </div>
     </div>
   );
 }
